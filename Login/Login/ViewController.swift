e72b0413b71848b561f8be4e36f0cb2cda03adef
MagnoQuezada <37008608+MagnoQuezada@users.noreply.github.com>
2018-05-31 12:05:48 -0500

    Add files via upload

---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+//import FirebaseAuth
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var loginEmailField: UITextField!
+    @IBOutlet weak var loginPasswordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func signUpTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}
+
